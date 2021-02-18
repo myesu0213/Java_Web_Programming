aca42561361c2cdd41e284868dd1f0eef06983b9
myesu0213 <77835648+myesu0213@users.noreply.github.com>
2021-02-18 14:59:30 +0900

    Revert "test1"
    This reverts commit 3235a1d82fc68ac3089dbcd49cd0805785d3661d.

--- javastudy/src/ch04/Prob04_01.java
+++
@@ -1,9 +0,0 @@
-package ch04;
-
-public class Prob04_01 {
-	public static void main(String[] args) {
-		int num1=10,num2=20,num3=30;
-		num1=num2=num3;
-		System.out.printf("%d %d %d",num1, num2,num3); // 30 30 30
-	}
-}
--- javastudy/src/ch3/StringConc.java
+++
@@ -1,7 +0,0 @@
-package ch3;
-
-public class StringConc {
-	public static void main(String[] args) {
-		System.out.println("asd"+true);
-	}
-}
